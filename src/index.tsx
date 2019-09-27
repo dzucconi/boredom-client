1b46e4384f134ea16878d1909439b9f47199399c
dzucconi <mail@damonzucconi.com>
2019-09-27 09:42:07 -0400

    Adds smoothscroll polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import smoothscroll from "smoothscroll-polyfill";
 
 import { App } from "./App";
 
 import "./index.css";
 
+smoothscroll.polyfill();
 ReactDOM.render(<App />, document.getElementById("root"));
